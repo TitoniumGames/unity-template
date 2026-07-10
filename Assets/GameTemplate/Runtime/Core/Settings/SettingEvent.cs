d2246c9d734e8343b7e6f6de7a809fbc32355c8f
Kien Nguyen <vankieen99@gmail.com>
2026-07-10 15:06:54 +0700

    feat: add setting template

---
+++ Assets/GameTemplate/Runtime/Core/Settings/SettingEvent.cs
@@ -0,0 +1,14 @@
+﻿using GameTemplate.Runtime.Core.WCore.EventBus;
+
+namespace GameTemplate.Runtime.Core.Settings
+{
+    public struct SettingEvent: IEvent
+    {
+        public GameSettings Settings { get; private set; }
+        
+        public SettingEvent(GameSettings settings)
+        {
+            Settings = settings;
+        }
+    }
+}
---
+++ Assets/GameTemplate/Runtime/Core/Settings/SettingManager.cs
@@ -0,0 +1,75 @@
+﻿using GameTemplate.Runtime.Core.WCore.EventBus;
+using Newtonsoft.Json;
+using UnityEngine;
+using WCore;
+
+namespace GameTemplate.Runtime.Core.Settings
+{
+    [System.Serializable]
+    public class GameSettings
+    {
+        public bool Music = true;
+        public bool Sfx = true;
+        public bool Haptic = true;
+    }
+    public class SettingManager: Singleton<SettingManager>
+    {
+        [SerializeField] private GameSettings settings;
+        [SerializeField] private bool syncOnStart = true;
+
+        private void Start()
+        {
+            if (syncOnStart)
+            {
+                SyncSettings();
+            }
+        }
+
+        public void SetMusic(bool music)
+        {
+            if (settings.Music == music)
+            {
+                return;
+            }
+            
+            settings.Music = music;
+            SaveSettings();
+        }
+
+        public void SetSfx(bool sfx)
+        {
+            if (settings.Sfx == sfx)
+            {
+                return;
+            }
+            
+            settings.Sfx = sfx;
+            SaveSettings();
+        }
+
+        public void SetHaptic(bool haptic)
+        {
+            if (settings.Haptic == haptic)
+            {
+                return;
+            }
+            settings.Haptic = haptic;
+            SaveSettings();
+        }   
+
+        private void SaveSettings()
+        {
+            PlayerPrefs.SetString("Settings", JsonConvert.SerializeObject(settings));
+            EventBus<SettingEvent>.Post(new SettingEvent(settings));
+        }
+        
+        public void SyncSettings()
+        {
+            settings = JsonConvert.DeserializeObject<GameSettings>(PlayerPrefs.GetString("Settings")) ?? new GameSettings();
+            SetMusic(settings.Music);
+            SetSfx(settings.Sfx);
+            SetHaptic(settings.Haptic);
+            SaveSettings();
+        }
+    }
+}
