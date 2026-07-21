50df01d7e4c830577166530f2c9e6bfe7aedf463
Kien Nguyen <vankieen99@gmail.com>
2026-07-21 15:58:02 +0700

    feat: add animation received component

---
+++ Assets/GameTemplate/Runtime/Core/WCore/AnimationReceived.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace WCore
+{
+    public class AnimationReceived: MonoBehaviour
+    {
+        public UnityEvent OnAnimationStarted;
+        public UnityEvent OnAnimationFinished;
+        
+        
+        public void AnimationStarted()
+        {
+            OnAnimationStarted?.Invoke();
+        }
+
+        public void AnimationFinished()
+        {
+            OnAnimationFinished?.Invoke();
+        }
+    }
+}
